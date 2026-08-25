b4eeb6ae2a40323b010df3937c775d2724f336b4
Nestah <mmanoti866@gmail.com>
2026-08-25 11:56:19 +0300

    feat: Ask's for price in Ksh  and applies  16% VAT

--- Test/week2_practice.py
+++ Test/week2_practice.py
@@ -4,3 +4,10 @@
 age = int(input("Enter your age: "))
 future_age = age + 5
 print(f"In 5 years, you will be {future_age} years old.")
+
+# 2. Ask for a price in KES, apply 16% VAT, print total
+price = float(input("Enter price in Ksh: "))
+total_price = price * 1.16
+print(f"Total price including 16% VAT: Ksh {total_price:.2f}")  #  :   tells python to stop treating it as variable.
+                                                                #  .2f  display the value to 2 decimal place.
+
