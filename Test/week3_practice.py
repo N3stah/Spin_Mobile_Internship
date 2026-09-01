283106304ca78b06f68051c51d8b421fdcdba9d9
Nestah <mmanoti866@gmail.com>
2026-09-01 08:45:40 +0300

    study: how to use string slicing

---
+++ Test/week3_practice.py
@@ -0,0 +1,7 @@
+#Deep Indexing & Slicing
+# 1. String Slicing
+sentence = "Spin Mobile Internship 2026"
+print("First 5 chars:", sentence[:5])
+print("Last 5 chars :", sentence[-5:])
+print("Reversed     :", sentence[::-1])
+
