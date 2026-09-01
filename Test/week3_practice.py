0c14cca3751f9165576a8d5180de77219c6c5446
Nestah <mmanoti866@gmail.com>
2026-09-01 08:55:19 +0300

    study: how to do list slicing

--- Test/week3_practice.py
+++ Test/week3_practice.py
@@ -5,3 +5,12 @@ print("First 5 chars:", sentence[:5])
 print("Last 5 chars :", sentence[-5:])
 print("Reversed     :", sentence[::-1])
 
+# 2. List Slicing
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+first_half = numbers[:5]
+second_half = numbers[5:]
+every_3rd = numbers[::3]
+
+print("\nFirst half :", first_half)
+print("Second half:", second_half)
+print("Every 3rd  :", every_3rd)
