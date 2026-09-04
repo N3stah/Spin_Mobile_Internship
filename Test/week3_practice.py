bfbb2a65457df263fb9854f4c980f41e9b351549
Nestah <mmanoti866@gmail.com>
2026-09-04 08:54:34 +0300

    feat: using FizzBuzz

--- Test/week3_practice.py
+++ Test/week3_practice.py
@@ -214,3 +214,15 @@ fast_time = time.perf_counter() - start_fast
 
 print(f"\nSlow duplicate check O(n^2): {slow_time:.6f} seconds")
 print(f"Fast duplicate check O(n)  : {fast_time:.6f} seconds")
+
+#FizzBuzz
+
+for i in range(1, 35):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
